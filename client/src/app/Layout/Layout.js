198f39f189f7b3ce744fd1c4672e9d27469af49e
muzikhan1 <muzeking393@gmail.com>
2023-12-13 09:01:28 +0500

    navbar completed

--- client/src/app/Layout/Layout.js
+++ client/src/app/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Component/Header'
 import Footer from '../Component/Footer'
+import Navbar from '../Component/Navbar'
 
 const Layout = ({ children }) => {
   return (
     <main className='w-full bg-white'>
       <Header />
+      <Navbar />
       <section className='w-full h-full'>{children}</section>
       <Footer />
     </main>
